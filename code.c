26726ca2bf5f782a485fbb683c3bdaaca9cf99ef
3words <44570796+3words@users.noreply.github.com>
2019-04-23 00:57:55 +0800

    Update code.c

--- code.c
+++ code.c
@@ -1,11 +1,12 @@
 
 #include <stdio.h>
-	int main() { 
-	  printf("?"); 
-	  int i;
-		for (i=0; i<10; i++)
-		{
-			printf("?");
-		}
-	  return 0; 
+int main() 
+{ 
+	printf("?"); 
+	int i;
+	for (i=0, i<10, i++)
+	{
+		printf("?");
 	}
+  return 0; 
+}
